16577fda28b2493cc526281052b8058af2b8d8de
EricHsu <erichsu1994123@gmail.com>
2020-03-14 14:03:59 +0800

    1st version

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    
+    public static void main(String[] args){
+
+        System.out.println("Hi，有沒有好吃的@_@" + args[0]);
+        System.out.println("Hi，有沒有好吃的>_<" + args[0]);
+    }
+
+
+
+}
